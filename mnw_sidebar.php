cf62c85bed79f6bdb4a3b0ef293eb702d926e0ae
Adrian Lang <mail@adrianlang.de>
2009-03-02 15:59:24 +0100

    Corrected SQL statement.

--- mnw_sidebar.php
+++ mnw_sidebar.php
@@ -10,11 +10,11 @@ add_action('init', 'mnw_subscribe_widget_register');
 function mnw_subscribe_widget($args) {
     extract($args);
     global $wpdb;
-    $count = $wpdb->get_var('SELECT COUNT(id) FROM ' . MNW_SUBSCRIBER_TABLE . ' GROUP BY id');
+    $count = $wpdb->get_var('SELECT COUNT(*) FROM ' . MNW_SUBSCRIBER_TABLE);
     echo $before_widget;
 ?>
         <div style="height: 45px; padding-left: 45px; background: transparent url(<?php echo get_template_directory_uri(); ?>/omb.png) no-repeat scroll left center;
-            text-align: right; font-size:180%;">
+            text-align: right; font-size:170%;">
             <?php printf(__ngettext('%d OMB subscriber', '%d OMB subscribers', $count, 'mnw'), $count); ?><br />
             <a href="<?php get_option('mnw_themepage_url'); ?>"><?php _e('Subscribe!', 'mnw'); ?></a>
         </div>
