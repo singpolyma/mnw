bd01f7e1d39a03fc946b575ff4fe1faf7c22aa2b
Adrian Lang <mail@adrianlang.de>
2009-06-01 12:39:36 +0200

    Add notices lists.

---
+++ mnw-notices-feed.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * This file is part of mnw.
+ *
+ * mnw - an OpenMicroBlogging compatible Microblogging plugin for Wordpress
+ * Copyright (C) 2009, Adrian Lang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+function mnw_notices_feed($type, $my_url, $notices) {
+    if ($type == 'sent') {
+        $title = __('Microblog posts by %s', 'mnw');
+    } else {
+        $title = __('Microblog posts to %s', 'mnw');
+    }
+
+    $updated = ($notices !== null) ? mysql2date('Y-m-d\TH:i:s\Z', $notices[0]['created']) : date('Y-m-d\TH:i:s\Z');
+    $html_url = preg_replace('/format=atom/', 'format=html', $my_url);
+
+    header('Content-Type: application/atom+xml; charset=' . get_option('blog_charset'), true);
+    echo '<?xml version="1.0" encoding="' . get_option('blog_charset') . '" ?' . '>';
+?>
+
+<feed
+    xmlns="http://www.w3.org/2005/Atom"
+    xml:lang="<?php echo get_option('rss_language'); ?>"
+    xmlns:thr="http://purl.org/syndication/thread/1.0"
+    <?php do_action('atom_ns'); ?>
+>
+    <id><?php echo attribute_escape($my_url); ?></id>
+    <link rel="self" href="<?php echo attribute_escape($my_url); ?>" type="application/atom+xml"/>
+    <link rel="alternate" href="<?php echo attribute_escape($html_url); ?>" type="<?php bloginfo_rss('html_type'); ?>" />
+
+    <title type="text"><?php echo ent2ncr(sprintf($title, get_bloginfo('title')));?></title>
+    <subtitle type="text"><?php bloginfo_rss('description'); ?></subtitle>
+<?php if ($type == 'sent') {?>
+    <author><name><?php echo get_option('omb_nickname');?></name></author>
+<?php } ?>
+
+    <updated><?php echo $updated; ?></updated>
+    <?php the_generator( 'atom' ); ?>
+
+<?php
+if ($notices !== null) {
+foreach($notices as $notice) {
+    $title = $notice['content'];
+    if (mb_strlen($title) > 140) {
+        $title = mb_substr($title, 0, 139) . '…';
+    }
+?>
+    <entry>
+        <id><?php echo htmlentities($notice['noticeurl']); ?></id>
+        <link rel="alternate" href="<?php echo attribute_escape($notice['noticeurl']); ?>" type="<?php bloginfo_rss('html_type'); ?>" />
+        <title><?php echo $title; ?></title>
+<?php if ($type != 'sent') {?>
+        <author><name><?php echo $notice['nickname'];?></name><uri><?php echo htmlentities($notice['authorurl']); ?></uri></author>
+<?php } ?>
+        <content><?php echo htmlentities($notice['content']); ?></content>
+        <updated><?php echo mysql2date('Y-m-d\TH:i:s\Z', $notice['created'], false); ?></updated>
+        <published><?php echo mysql2date('Y-m-d\TH:i:s\Z', $notice['created'], false); ?></published>
+    </entry>
+<?php }
+} ?>
+</feed>
+<?php
+    return array(false, array());
+}
+?>
---
+++ mnw-notices.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * This file is part of mnw.
+ *
+ * mnw - an OpenMicroBlogging compatible Microblogging plugin for Wordpress
+ * Copyright (C) 2009, Adrian Lang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once 'lib.php';
+
+function mnw_notices() {
+    /* Parse input type request. */
+    $types = array(
+        'sent' => 'SELECT *, CONCAT("' . mnw_set_action('get_notice') . '&mnw_notice_id=' . '", id) AS "noticeurl" FROM ' . MNW_NOTICES_TABLE . ' AS notice',
+        'received' => 'SELECT *, author.url AS "authorurl", notice.url as "noticeurl" FROM ' . MNW_FNOTICES_TABLE . ' AS notice, ' .
+                    MNW_SUBSCRIBER_TABLE . ' AS author ' . 
+                    'WHERE to_us = 1 AND notice.user_id = author.id');
+
+    if (isset($_GET['type']) && isset($types[$_GET['type']])) {
+        $type = $_GET['type'];
+    } else {
+        $type = 'sent';
+    }
+
+    /* Parse output type request. */
+    $formats = array(
+        'html' => array('mnw-notices-html.php', 'mnw_notices_html'),
+        'atom' => array('mnw-notices-feed.php', 'mnw_notices_feed'));
+    if (isset($_GET['format']) && isset($formats[$_GET['format']])) {
+        $format = $_GET['format'];
+    } else {
+        $format = 'html';
+    }
+
+    /* Get offset. */
+    if (isset($_GET['offset'])) {
+        $offset = (int) $_GET['offset'];
+    } else {
+        $offset = 0;
+    }
+
+    /* Get notices. */
+    global $wpdb;
+    $notices = $wpdb->get_results($types[$type] . ' ' .
+                                  'ORDER BY notice.created DESC ' . 
+                                  'LIMIT ' . $offset . ', 10', ARRAY_A);
+
+    $my_url = mnw_set_action('notices') . "&type=$type" . ($offset != 0 ? "&offset=$offset" : '') . "&format=$format";
+
+    /* Send notices to output engine. */
+    require_once $formats[$format][0];
+    return $formats[$format][1]($type, $my_url, $notices);
+}
+?>
